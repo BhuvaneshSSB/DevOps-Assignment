535a5cfc6388a01cea8ed298a8cc9eb869355955
BhuvaneshSSB <bhuvaneshssb@gmail.com>
2026-02-24 20:02:48 +0000

    completed: terraform for aws infra

---
+++ infrastructure/terraform/aws/main.tf
@@ -0,0 +1,35 @@
+# Path: infrastructure/terraform/aws/main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-aws-devops-1771960923"
+    key            = "devops-assignment/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = "devops-assignment"
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "fallback"
+  region = var.aws_region_fallback
+}
